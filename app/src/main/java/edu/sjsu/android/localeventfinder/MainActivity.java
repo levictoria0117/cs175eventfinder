3c8bf7130c9586fd904a72fbbb851c5d7b8b7bdf
erik-ksth <erikkizme@gmail.com>
2024-11-11 02:33:35 -0800

    feat: tab-bar and page navigation

--- app/src/main/java/edu/sjsu/android/localeventfinder/MainActivity.java
+++ app/src/main/java/edu/sjsu/android/localeventfinder/MainActivity.java
@@ -7,18 +7,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new HomeFragment(), "Home");
+        vpAdapter.addFragment(new FavoritesFragment(), "Favorite");
+        vpAdapter.addFragment(new TicketsFragment(), "Tickets");
+        vpAdapter.addFragment(new SettingsFragment(), "Settings");
+        viewPager.setAdapter(vpAdapter);
+
+        setupTabIcons();
+    }
+
+    private void setupTabIcons() {
+        Objects.requireNonNull(tabLayout.getTabAt(0)).setIcon(R.drawable.home_icon);
+        Objects.requireNonNull(tabLayout.getTabAt(1)).setIcon(R.drawable.favorites_icon);
+        Objects.requireNonNull(tabLayout.getTabAt(2)).setIcon(R.drawable.tickets_icon);
+        Objects.requireNonNull(tabLayout.getTabAt(3)).setIcon(R.drawable.account_icon);
+
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            TabLayout.Tab tab = tabLayout.getTabAt(i);
+            if (tab != null) {
+                tab.setText(null);
+            }
+        }
     }
 }
