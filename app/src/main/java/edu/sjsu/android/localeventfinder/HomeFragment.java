137a69ccb7b93372bc7c44b2bfa3dbf171b9a85a
Victoria Le <95896163+levictoria0117@users.noreply.github.com>
2024-11-28 02:54:36 -0800

    Update HomeFragment.java

--- app/src/main/java/edu/sjsu/android/localeventfinder/HomeFragment.java
+++ app/src/main/java/edu/sjsu/android/localeventfinder/HomeFragment.java
@@ -10,17 +10,44 @@ import android.view.ViewGroup;
 
 public class HomeFragment extends Fragment {
 
+    private RecyclerView nearestEventsRecyclerView, moreEventsRecyclerView;
+    private MyAdapter nearestEventsAdapter, moreEventsAdapter;
+    private ArrayList<Event> nearestEventsList, moreEventsList;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-        }
+
+        nearestEventsList = new ArrayList<>();
+        nearestEventsList.add(new Event(R.drawable.event_temp_background, R.string.event_name_1, R.string.event_location_1, R.string.event_date_1, R.string.event_desc_1));
+        nearestEventsList.add(new Event(R.drawable.event_temp_background, R.string.event_name_2, R.string.event_location_2, R.string.event_date_2, R.string.event_desc_2));
+        nearestEventsList.add(new Event(R.drawable.event_temp_background, R.string.event_name_3, R.string.event_location_3, R.string.event_date_3, R.string.event_desc_3));
+        nearestEventsList.add(new Event(R.drawable.event_temp_background, R.string.event_name_4, R.string.event_location_4, R.string.event_date_4, R.string.event_desc_4));
+        nearestEventsList.add(new Event(R.drawable.event_temp_background, R.string.event_name_5, R.string.event_location_5, R.string.event_date_5, R.string.event_desc_5));
+
+        moreEventsList = new ArrayList<>();
+        moreEventsList.add(new Event(R.drawable.event_temp_background, R.string.event_name_1, R.string.event_location_1, R.string.event_date_1, R.string.event_desc_1));
+        moreEventsList.add(new Event(R.drawable.event_temp_background, R.string.event_name_2, R.string.event_location_2, R.string.event_date_2, R.string.event_desc_2));
+        moreEventsList.add(new Event(R.drawable.event_temp_background, R.string.event_name_3, R.string.event_location_3, R.string.event_date_3, R.string.event_desc_3));
+        moreEventsList.add(new Event(R.drawable.event_temp_background, R.string.event_name_4, R.string.event_location_4, R.string.event_date_4, R.string.event_desc_4));
+        moreEventsList.add(new Event(R.drawable.event_temp_background, R.string.event_name_5, R.string.event_location_5, R.string.event_date_5, R.string.event_desc_5));
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        moreEventsRecyclerView = view.findViewById(R.id.more_events_recycler);
+        moreEventsRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        moreEventsAdapter = new MyAdapter(moreEventsList);
+        moreEventsRecyclerView.setAdapter(moreEventsAdapter);
+
+        nearestEventsRecyclerView = view.findViewById(R.id.nearest_events_recycler);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2, LinearLayoutManager.HORIZONTAL, false);
+        nearestEventsRecyclerView.setLayoutManager(gridLayoutManager);
+        nearestEventsAdapter = new MyAdapter(nearestEventsList);
+        nearestEventsRecyclerView.setAdapter(nearestEventsAdapter);
+
+        return view;
     }
 }
