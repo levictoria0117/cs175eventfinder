685ab05cf44652f4f1af5061157db5f216e8578f
GuadalupeRamirezLara <guadalupe.ramirezlara@sjsu.edu>
2024-12-02 22:25:53 -0800

    button functionally added, no DB yet

--- app/src/main/java/edu/sjsu/android/localeventfinder/LandingPageActivity.java
+++ app/src/main/java/edu/sjsu/android/localeventfinder/LandingPageActivity.java
@@ -1,6 +1,9 @@
 package edu.sjsu.android.localeventfinder;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,14 @@ public class LandingPageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_landing_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button signInButton = findViewById(R.id.signin_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LandingPageActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
--- app/src/main/java/edu/sjsu/android/localeventfinder/LoginActivity.java
+++ app/src/main/java/edu/sjsu/android/localeventfinder/LoginActivity.java
@@ -1,6 +1,9 @@
 package edu.sjsu.android.localeventfinder;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,16 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Button signInButton = findViewById(R.id.signin_button);
+        Button guestButton = findViewById(R.id.guest_button);
+        View.OnClickListener navigateToMain = v -> {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        };
+
+        signInButton.setOnClickListener(navigateToMain);
+        guestButton.setOnClickListener(navigateToMain);
     }
 }
