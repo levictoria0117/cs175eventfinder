373553c322961a66da17161c8a9a9daffb98f01a
erik-ksth <erikkizme@gmail.com>
2024-12-09 18:40:22 -0800

    update: redirect to google map

--- app/src/main/java/edu/sjsu/android/localeventfinder/TicketDetailActivity.java
+++ app/src/main/java/edu/sjsu/android/localeventfinder/TicketDetailActivity.java
@@ -1,10 +1,12 @@
 package edu.sjsu.android.localeventfinder;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -35,6 +37,9 @@ public class TicketDetailActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        MaterialButton getDirectionButton = findViewById(R.id.getDirectionButton);
+        getDirectionButton.setOnClickListener(v -> openGoogleMaps());
+
         Intent intent = getIntent();
         event = intent.getParcelableExtra("event");
 
@@ -56,4 +61,32 @@ public class TicketDetailActivity extends AppCompatActivity {
             eventTime.setText(event.getDateID());
         }
     }
+
+    private void openGoogleMaps() {
+        if (event != null) {
+            try {
+                String location = "1 Washington Square, San Jose, CA 95192";
+
+                // Create a URI for Google Maps with the location
+                Uri gmmIntentUri = Uri.parse("google.navigation:q=" + Uri.encode(location));
+
+                // Create an Intent to open Google Maps
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+
+                // Verify that Google Maps is installed
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                } else {
+                    // If Google Maps isn't installed, open in browser
+                    Uri browserUri = Uri.parse("https://www.google.com/maps/search/?api=1&query=" + Uri.encode(location));
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, browserUri);
+                    startActivity(browserIntent);
+                }
+            } catch (Exception e) {
+                Toast.makeText(this, "Unable to open maps", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
